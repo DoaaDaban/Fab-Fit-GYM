9ca3b3f3148e2cabe2930d94b18cea53c4cb0c49
DoaaDaban <>
2021-05-05 20:25:27 +0000

    add js file

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+var home = 'Home page';
+var classes = 'classes we have';
+var location = 'MAP';
+var join_us='welcome to our family';
+
+var setUrPlane=prompt ('what is ur goal?');
+//  // console.log(setUrPlane);
+
+
+if (setUrPlane =='lose fat'){
+document.write('<img src="https://www.pickthebrain.com/blog/wp-content/uploads/2013/10/Workout.jpg">');
+}
+else if(setUrPlane =='get fit'){
+document.write('<h3> here we go </h3>');
+}
+else{
+  document.write('<h4> Have fun </h4>');
+}
+
+
+//console.log(home);
+
+//alert(home);
+
+//document.write('<h3>'+home+'<h3>');
+
+
+//if(home==true){}
