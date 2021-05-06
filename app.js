3abed4d59f2876bef110e4029c36b8d92e7f863e
DoaaDaban <>
2021-05-06 10:25:25 +0000

    jss file

--- app.js
+++ app.js
@@ -11,32 +11,23 @@ var name=prompt ('what is ur name?');
 var age=prompt ('what is ur age?');
 
 
-<<<<<<< HEAD
+
 
 var setUrPlane=prompt ('what is ur goal? lose fat or get fit?');
 //  // console.log(setUrPlane);
-if (setUrPlane =='lose fat'){
+if (setUrPlane =='lose fat')
+{
 document.write('<h2> you are on ur way </h2>');
-=======
+}
 var setUrPlane=prompt ('what is ur goal? lose fat or get fit?');
 //  // console.log(setUrPlane);
-if (setUrPlane =='lose fat'){
+if (setUrPlane =='lose fat')
+{
 document.write('<h4> you are on ur way </h4>');
->>>>>>> 5805a08fdb0b081695c692eabd231dfd9f2d4434
 }
 else if(setUrPlane =='get fit'){
 document.write('<h2> here we go </h2>');
 }
 else{
-  document.write('<img src="https://app.retargetbot.com/upload/comboposter/2/2_image_1582136788872399.jpg">');
-}
-
-
-//console.log(home);
-
-//alert(home);
-
-//document.write('<h3>'+home+'<h3>');
-
-
-//if(home==true){}
+  document.write('<img src="https://app.retargetbot.com/upload/comboposter/2/2_image_1582136788872399.jpg">')
+  }
