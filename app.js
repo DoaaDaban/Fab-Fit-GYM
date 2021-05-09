6681ac6fd914e5b1bb2e7130f448c5299062e7ff
DoaaDaban <>
2021-05-09 12:04:37 +0000

    addFunc

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
-var name=prompt ('please enter ur weight?');
-var age=prompt ('please enter ur height?');
+var weight =prompt ('please enter ur weight?');
+var height =prompt ('please enter ur height?');
 
 var setUrPlane=prompt ('what is ur goal? lose fat or get fit?');
 
@@ -10,28 +10,39 @@ if (setUrPlane =='lose fat')
 document.write('<h4> you are on ur way </h4>');
 }
 
-else if(setUrPlane =='get fit'){
+else if(setUrPlane =='get fit')
+{
 document.write('<h4> here we go! </h4>');
 }
 
-else{
-
- document.write('<img src="https://cdn4.vectorstock.com/i/1000x1000/78/18/healthy-heart-cardio-icon-vector-12827818.jpg" width="50 px">')
-
-}
+function planeSetUp (weigh, height){
 
 while (setUrPlane !== "lose fat" && setUrPlane !== "get fit"){
 setUrPlane=prompt ('pls choose ur goal? lose fat or get fit?');
 }
+var plane=document.write("ur plane set up uploading");
+return plane;
+}
 
+
+
+var getRating =function(){
+
+var result='';
 var rate= prompt('please rate our gym out of 5?');
 
 for( var i=1; i<=rate; i++){
 
-  if (i<=5){
+ if (i<=5){
   document.write('<img src="https://previews.123rf.com/images/travelman/travelman1610/travelman161000052/64334690-healthy-heart-icon-heart-with-healthy-status-check-mark-and-cardiogram-.jpg" width=50px>');
   }
   else {
-    var note=prompt('please enter rate up to 5 only');
+    rate=prompt('please enter rate up to 5 only');
   }
 }
+return result;
+}
+
+planeSetUp("53","158");
+
+getRating();
